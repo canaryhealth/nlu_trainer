40c3dd758fb6fc7e30e156745561067a75205ef3
tinman6 <tinman6@users.noreply.github.com>
2016-09-23 11:53:53 -0400

    added "purge" command to cli

--- cli.py
+++ cli.py
@@ -12,16 +12,16 @@ import asset
 
 TRAIN = 'train'
 TEST  = 'test'
-
+PURGE = 'purge'
 
 def main(args=None):
   cli = argparse.ArgumentParser(
     description=__doc__,
-    epilog='ex: python cli.py train settings.ini train_set.txt')
-  cli.add_argument('command', metavar='COMMAND', help='{ train | test }',
-                   choices=(TRAIN, TEST))
+    epilog='ex: python cli.py train settings.ini -d train_set.csv')
+  cli.add_argument('command', metavar='COMMAND', help='{ train | test | purge }',
+                   choices=(TRAIN, TEST, PURGE))
   cli.add_argument('config', metavar='CONFIG', help='config file with settings')
-  cli.add_argument('dataset', metavar='DATASET', help='dataset file')
+  cli.add_argument('-d', '--dataset', metavar='DATASET', help='dataset file')
 
   options  = cli.parse_args(args=args)
   cp = ConfigParser.RawConfigParser()
@@ -29,12 +29,30 @@ def main(args=None):
   settings = aadict(cp.items('nlu_trainer'))
 
   trainer = asset.symbol(settings.driver)(settings)
-  with open(options.dataset) as csv_file:
-    r = csv.reader(csv_file)
-    if options.command == TRAIN:
-      trainer.train_set(r)
-    elif options.command == TEST:
-      trainer.test_set(r)
+  if options.command in (TRAIN, TEST):
+    with open(options.dataset) as csv_file:
+      r = csv.reader(csv_file)
+      if options.command == TRAIN:
+        trainer.train_set(r)
+      elif options.command == TEST:
+        trainer.test_set(r)
+  elif options.command == PURGE:
+    purge(trainer)
+
+
+def purge(trainer):
+  # delete examples
+  for e_id in [ x['exampleId'] for x in trainer.get_examples() ]:
+    trainer.delete_example(e_id)
+  # delete intent
+  for i in trainer.get_intents():
+    if i['name'] != 'None':
+      trainer.delete_intent(i['id'])
+  # delete entities
+  for e_id in [ x['id'] for x in trainer.get_entities() ]:
+    trainer.delete_entity(e_id)
+
+  trainer.update()
 
 #------------------------------------------------------------------------------
 main()
