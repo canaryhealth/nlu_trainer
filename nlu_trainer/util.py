5c7a8e65c349b625b720f817f61bbfe03d8fa898
tinman6 <tinman6@users.noreply.github.com>
2017-01-05 22:18:27 -0500

    added phrase_pos to location start and end position of phrase

--- nlu_trainer/util.py
+++ nlu_trainer/util.py
@@ -17,3 +17,25 @@ def phrase_index(sentence, phrase):
     l = len(m.group(1))
     return (l, l+len(phrase)-1)
   return None
+
+
+def phrase_pos(sentence, phrase):
+  '''
+  Returns the start and end position of phrase (first instance) if it exists in
+  sentence.
+
+    ex: >>> phrase_index('the quick brown fox jumps over the lazy dog',
+                         'brown fox jumps')
+        (2, 5)
+  '''
+  phrase = str(phrase)  # in case phrase is a number
+  s_tok = sentence.split()
+  p_tok = phrase.split()
+  p_len = len(p_tok)
+
+  # get all indices where s_tok[i] matches p_tok[0]
+  indices = [ i for i, x in enumerate(s_tok) if x == p_tok[0] ]
+  for i in indices:
+    if s_tok[i : i+p_len] == p_tok:
+      return i, i+p_len
+  return None
